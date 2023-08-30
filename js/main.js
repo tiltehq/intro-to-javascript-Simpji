ec7fb00003f0f789475ba52fc7a1ed57ac240c64
william chinonso <williammschinonso20@gmail.com>
2023-08-30 14:31:00 -0700

    Quiz project

--- js/main.js
+++ js/main.js
@@ -274,6 +274,12 @@ trueButton.addEventListener("click", (event) =>{
     trueButton.textContent =
     trueButton.textContent.toUpperCase();
 });
+h1.addEventListener("mouseover", () => {
+    h1.textContent = "hearing";
+});
+h1.addEventListener("mouseout", () => {
+    h1.textContent = "Quiz.js";
+});
 
 // Conditionals
 if (you.WannaBeMyLover) {
