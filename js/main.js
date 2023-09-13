0a660f1cd10b2c179b69c9cc7afebd11a347fe57
william chinonso <williammschinonso20@gmail.com>
2023-09-13 12:46:17 -0700

    my new commit

--- js/main.js
+++ js/main.js
@@ -312,3 +312,244 @@ if (isCorrect(button.value)) {
 }else{
     button.classlist.add("incorrect");
 }
+
+// map & filter
+const spices = [
+    {name: "simple", nickname: "simp"},
+    {name: "emma", nickname: "baby"},
+    {name: "good", nickname: "bell"},
+    {name: "sunny", nickname: "sons"},
+    {name: "key", nickname: "will"},
+];
+const nicknames = spices.map(s => s.nickname + "spice");
+let spices2 = spices.map(s => `${s.nickname} Spice`);
+const mels = spices.filter(s => s.name.includes("mels"));
+const names = spices.map(spice => spice.name);
+const names = spices.map(spice => spice.name);
+undefined
+names
+let endInY = spices.filter(s => s.name.endsWith("y"))
+undefined
+endInY
+endInY = spices.filter(s => s.nickname.endsWith("y"))
+endInY = spices.filter(s => s.nicknames.endsWith("y"))
+
+endInY
+"stringy"[0]
+'s'
+"stringy"[1]
+'t'
+
+// spread
+const oldBurns = ["square", "wack",];
+const newBurns = ["basic", "dusty", "sus"];
+const burnBook = [...newBurns, ...oldBurns];
+undefined
+burnBook
+(5) ['basic', 'dusty', 'sus', 'square', 'wack']
+
+const skills = ["Html", "css", "js"];
+const newSkills = ["React", "TypeScrip", "Node"];
+
+// While Loop
+let fiveRandomNumbers = [];
+while (fiveRandomNumbers.length < 5) {
+    fiveRandomNumbers.push(Math.random());
+}
+
+let sevenRandomNumbers = [];
+while (sevenRandomNumbers.length < 7) {
+    sevenRandomNumbers.push(Math.random());
+}
+ while (true) {
+     console.log("1 am wasting resources infinitely");
+ }
+
+//  Doggo Quiz while Review
+
+ function getMultipleChoices(n, correctAnswer, array) {
+            // Use a while loop and the getRandomElement() function
+            // Make sure there are no duplicates in the array
+            const choices = [correctAnswer];
+            while (choices.length < n) {
+                let candidate = getRandomElement(array);
+                if (choices.indexOf(candidate) < 0) { // check if this is already in the array
+                    choices.push(candidate); // if not, add it
+                }
+            }
+            return shuffleArray(choices);
+        }
+undefined
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['underfined', 'number', 'boolean']
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['boolean', 'number', 'string']
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['underfined', 'boolean', 'string']
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['number', 'underfined', 'boolean']
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['string', 'boolean', 'number']
+getMultipleChoices(3, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(3) ['number', 'boolean', 'underfined']
+getMultipleChoices(4, "boolean", ["boolean", "string", "object", "number", "underfined"])
+(4) ['underfined', 'boolean', 'string', 'number']
+getMultipleChoices(3, "string", ["boolean", "string", "object", "number", "underfined"])
+(3) ['string', 'object', 'underfined']
+getMultipleChoices(4, "string", ["boolean", "string", "object", "number", "underfined"])
+(4) ['boolean', 'number', 'object', 'string']
+
+  
+console.log("this will print in a new York minute");
+console.log("this will print one new York minute later"); 
+// this will print in a new York minute
+// this will print one new York minute later
+undefined
+
+setTimeout
+console.log("This will print first");
+setTimeout(() => console.log("will print third"), 1000);
+console.log("This will print second");
+// This will print first
+// This will print second
+undefined
+// will print third
+
+APIS & fetch
+// APIS provide ERLS that point at data we care about
+// https://dog.ceo/api/breed/hound/list
+
+// Working with Promise
+fetch ("https://dog.ceo/api/breed/hound/list")
+ 
+// Using await with Promise
+let response = await fetch ("https://dog.ceo/api/breed/hound/list")
+console.log(response);
+let response = await fetch ("https://dog.ceo/api/breed/hound/list")
+let body = await response.json();
+
+// Destructuring Object & Arrays
+// Destructuring is a fancy way of declaring multiple variable at once
+const spices = [
+    {name: "simple", nickname: "will"},
+]
+let {name, nickname} = spices[0];
+let {nickname} = spices[0];
+undefined
+nickname
+'will'
+name
+'simple'
+let [one, two, three] = [1, 2, 3]
+undefined
+one
+1
+three
+3
+
+// Destructuring Exercise
+"simple sunday williams".split(" ");
+(3) ['simple', 'sunday', 'williams']
+
+let [first, middle, last] = " simple sunday williams".spLit(" ");
+
+// Destructuring Solution: Return Breed
+"https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg"
+'https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg'
+"https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg".split("/")[4]
+'poodle-standard'
+"https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg".split("breed/")
+['https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg']
+"https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg".split("/")[4]
+'poodle-standard'
+'poodle-standard'.split("_")
+"poodle-standard".split("_")
+['poodle-standard']
+"beagle".split("_")
+['beagle']
+"poodle-standard".split("_").join(" ")
+'poodle-standard'
+"https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg".split("/")[4]
+'poodle-standard'
+let [a,b] = "poodle-standard".split("_")
+undefined
+a
+'poodle-standard'
+b
+undefined
+[a, b]
+(2) ['poodle-standard', undefined]
+let [breed, varienty] = "poodle-standard".split("_")
+undefined
+[varienty, breed]
+(2) [undefined, 'poodle-standard']
+[varienty, breed].join(" ")
+' poodle-standard'
+let splitArray = "poodle-standard".split("_")
+undefined
+splitArray
+['poodle-standard']
+splitArray.reverse()
+['poodle-standard']
+
+// Aync Functions
+let response = await fetch ("https://dog.ceo/api/breed/hound/list")
+undefined
+response
+// Response {type: 'cors', url: 'https://dog.ceo/api/breed/hound/list', redirected: false, status: 200, ok: true, …}
+let body = await response.json();
+undefined
+body
+// {message: Array(7), status: 'success'}message: (7) ['afghan', 'basset', 'blood', 'english', 'ibizan', 'plott', 'walker']status: "success"[[Prototype]]: Object
+let { message } = body;
+undefined
+message
+(7) ['afghan', 'basset', 'blood', 'english', 'ibizan', 'plott', 'walker']
+function fetchResponse(url) {
+    const response = await fetch(url);
+    return response;
+}
+
+async function fetchResponse(url) {
+    const response = await fetch(url);
+    return response;
+}
+
+// Async Function Exercise
+async function fetchMessage(url) {
+    const response = await fetch(url);
+    const body = await response.json();
+    const {message} = body;
+    return message;
+}
+undefined
+await fetchMessage("https://dog.ceo/api/breed/hound/list")
+(7) ['afghan', 'basset', 'blood', 'english', 'ibizan', 'plott', 'walker']
+
+// Adding Choices Button Solution
+for (let choice of choicesArray) {
+    const button = document.createElement("button");
+    button.textContent = choice;
+    button.value = choice;
+    button.name = choice;
+    button.addEventListener("click", buttonHandler);
+    options.appendChild(button);
+}
+
+// Render Quiz Exercise
+const [imageUrl, correctAnswer, choices] = await loadQuizData();
+        renderQuiz(imageUrl, correctAnswer, choices);
+
+// Debugger
+function whyIsntThisWorking(input) {
+    debugger;
+    return thingThatDoesntWork(input);
+}
+try {
+    thisMightThrowAnError();
+}catch(error) {
+    console.log("as if! error", error);
+    console.log("Whatever, let's press on anyway");
+}
+console.log("still rollin' with the homies");
